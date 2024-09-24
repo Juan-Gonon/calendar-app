7f1fd9846259a888e01651ec6a914b6e4c9f2cc9
Juan Gonon <juanfegonon27@gmail.com>
2024-09-24 11:09:24 -0600

    despachar acciones respectivas login

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,19 +1,27 @@
 import { useDispatch, useSelector } from "react-redux";
 import { calendarApi } from "../api";
+import { clearErrorMessage, onChecking, onLogin, onLogout } from "../store";
 
 export const useAuthStore = () => {
     const { status, user, errorMessage } = useSelector((state) => state.auth);
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
     const startLogin = async ({ email, password }) => {
+        dispatch(onChecking())
         try {
-            const res = await calendarApi.post("/auth", {
+            const { data } = await calendarApi.post("/auth", {
                 email,
                 password,
             });
-            console.log(res);
+            localStorage.setItem('token', data.token )
+            localStorage.setItem('token-init-date', new Date().getTime())
+
+            dispatch(onLogin({ name: data.name, uid: data.uid }))
         } catch (error) {
-            console.log(error);
+            dispatch(onLogout('Credenciales incorrectas', error.message))
+            setTimeout(() => {
+                dispatch(clearErrorMessage())
+            }, 10)
         }
     };
 
